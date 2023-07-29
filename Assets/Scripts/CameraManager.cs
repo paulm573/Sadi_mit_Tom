bfe9f96ee0b2fc613fcaebb8bcf910801da67750
glBindBuffer <66163620+paulm573@users.noreply.github.com>
2023-07-29 14:56:41 +0200

    Revert "working Logic from Image to World"
    This reverts commit 425b208e782d614c9eb70a87ae0e5495f3d6310e.

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.IO.Enumeration;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,9 +11,8 @@ public class CameraManager : MonoBehaviour
     private WebCamTexture WebCamTexture;
     [SerializeField]
     private RawImage cameraPreview;
-    Texture2D takenImage;
-    [SerializeField,Tooltip("name of the file where the image as map for WorldGeneration is stored")]
-    string fileName;
+
+    private Texture snapShotPreview;
 
     private void Start() {
         webcamDevices = WebCamTexture.devices;
@@ -33,37 +31,35 @@ public class CameraManager : MonoBehaviour
     }
 
     public void TakePicture() {
+     //   WebCamTexture.Stop();
         Debug.Log("took picture");
         GameObject ssp = GameObject.Find("PicturePreview");
         if (ssp == null) {
             throw new System.Exception("no such Gameobject found: PicturePreview");
         }
-        takenImage = new Texture2D(WebCamTexture.width, WebCamTexture.height);
+        Texture2D takenImage = new Texture2D(WebCamTexture.height, WebCamTexture.height);
         takenImage.SetPixels(WebCamTexture.GetPixels());
         takenImage = createImage(takenImage);
         takenImage.Apply();
         ssp.GetComponent<RawImage>().texture = takenImage;
-      
-        WebCamTexture.Stop();
+        //TODO -> fixen, pixel werden angezeigt, aber nicht in der richtigen Reihenfolge!
+
+
     }
 
     private Texture2D createImage(Texture2D tex) {
      
         Texture2D result = new Texture2D(WebCamTexture.height, WebCamTexture.height); // create squared Array for squared Image
       
-        int spaceToRemove =(WebCamTexture.width - WebCamTexture.height) / 2;
-        Debug.Log("leftBorder: " + spaceToRemove + " rightBorder: " + (WebCamTexture.width - spaceToRemove));
-        Debug.Log("new width: " + (WebCamTexture.width - spaceToRemove - spaceToRemove));
-        Debug.Log("width: " + WebCamTexture.width + "height: " + WebCamTexture.height);
-       // Color[] tmp = new Color[WebCamTexture.height];
+        int spaceToRemove = WebCamTexture.width / 4;
+        Debug.Log("leftBorder: " + spaceToRemove + "rightBorder: " + (WebCamTexture.width - spaceToRemove) + "width: " + (WebCamTexture.width - spaceToRemove));
         for (int x = spaceToRemove; x < WebCamTexture.width - spaceToRemove; x++) { // create the squared array
             for (int y = 0; y < WebCamTexture.height; y++) {
-                int loc = x + (y * WebCamTexture.width);
-                result.SetPixel(x-spaceToRemove, y, tex.GetPixel(x, y));
+                int loc = x + (y * WebCamTexture.width); // get location in 1d array
+                result.SetPixel(x, y, tex.GetPixel(x,y));
             }
         }
-       // result.SetPixels(tmp); 
-
+      
         return result;
 
     }
@@ -71,14 +67,6 @@ public class CameraManager : MonoBehaviour
     public void RemovePicture() {
         WebCamTexture.Play();
         Debug.Log("remove picture");
-    }
-
-     public void ContinueWithThisPicture() {
-        if (takenImage != null) {
-            byte[] byteArray = takenImage.EncodeToPNG();
-            System.IO.File.WriteAllBytes(Application.dataPath + "/Resources/" + fileName + ".png", byteArray);
-            Debug.Log("written the image to " + Application.dataPath + "/Resources/"); 
-        }
     }
 
 }
