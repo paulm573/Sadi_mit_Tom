398d9f9d03a10deea3022573b94577bbcfe4cbf8
glBindBuffer <66163620+paulm573@users.noreply.github.com>
2023-08-04 13:56:27 +0200

    m

--- Assets/Scripts/WorldBuilder.cs
+++ Assets/Scripts/WorldBuilder.cs
@@ -3,33 +3,43 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using Unity.Mathematics;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 
 public class WorldBuilder : MonoBehaviour
 {
-    public string path = "";
-    GameObject wall, red, orange, blue, floor;
+    
+    public string path_prefabs = "";
+    GameObject wall, red, orange, blue, floor, player;
     List<(int, int)> legalSpawns;
-    [SerializeField]Transform playerTransform;
+    private GameObject world;
+    private Vector3 playerSpawn;
 
     private void Awake()
     {
-        this.transform.localScale= Vector3.one;
+        playerSpawn = Vector3.zero;
+        world = new GameObject();
+        world.transform.localScale= Vector3.one;
+        world.transform.position= Vector3.zero;
+        world.transform.rotation= Quaternion.identity;
         LoadPrefabs();
     }
 
     private void LoadPrefabs()
     {   
-        red = (GameObject)Resources.Load(path + "/" + "red");
-        orange = (GameObject)Resources.Load(path + "/" + "orange");
-        blue = (GameObject)Resources.Load(path + "/" + "blue");
-        wall = (GameObject)Resources.Load(path + "/" + "wall");
-        floor = (GameObject)Resources.Load(path + "/" + "floor");
+        red = (GameObject)Resources.Load(path_prefabs + "/"+ "red");
+        orange = (GameObject)Resources.Load(path_prefabs + "/" + "orange");
+        blue = (GameObject)Resources.Load(path_prefabs + "/" + "blue");
+        wall = (GameObject)Resources.Load( path_prefabs + "/" + "wall");
+        floor = (GameObject)Resources.Load(path_prefabs + "/" + "floor");
+        player = (GameObject)Resources.Load(path_prefabs + "/" + "playerOne");
         legalSpawns = new List<(int, int)>();
        
     }
+
+    public Vector3 GetPlayerSpawn() { return playerSpawn; }
 
     public void CreateWorld(int nipScale,int nipCount_I, int nipCount_J, Stack<WorldObject> worldObjects) 
     {
@@ -56,10 +66,13 @@ public class WorldBuilder : MonoBehaviour
         float floorX = (nipScale * nipCount_I) / 2 - nipScale / 2;
         float floorY = (nipScale / 2 * -1) - 0.5f;
         float floorZ = (nipScale * nipCount_J) / 2 - nipScale / 2;
-        Instantiate(floor, new Vector3(floorX, floorY ,floorZ ), Quaternion.Euler(rotation)).transform.SetParent(this.transform);
+        Instantiate(floor, new Vector3(floorX, floorY ,floorZ ), Quaternion.Euler(rotation)).transform.SetParent(world.transform);
         // place player
         (int x, int z) = legalSpawns[UnityEngine.Random.Range(0, legalSpawns.Count)];
-        playerTransform.position = new Vector3(x * nipScale, 10f, z * nipScale);
+        playerSpawn = new Vector3(x * nipScale, 3f, z * nipScale);
+        // Instantiate(player, position_player, Quaternion.identity).transform.SetParent(world.transform);
+        SaveWorldAsPrefab();
+        Instantiate((GameObject)Resources.Load("generatedWorld"),  Vector3.zero, Quaternion.identity);
     }
 
     private void SetScale(int nipScale, int nipCount_I, int nipCount_J)
@@ -75,10 +88,23 @@ public class WorldBuilder : MonoBehaviour
     private void InstaniateObject(string type, Vector3 pos, Vector3 rot) {
         switch (type)
         {
-            case "wall": Instantiate(wall, pos, Quaternion.Euler(rot)).transform.SetParent(this.transform); break;
-            case "orange": Instantiate(orange, pos, Quaternion.Euler(rot)).transform.SetParent(this.transform); break;
-            case "blue": Instantiate(blue, pos, Quaternion.Euler(rot)).transform.SetParent(this.transform); break;
-            case "red": Instantiate(red, pos, Quaternion.Euler(rot)).transform.SetParent(this.transform); break;
+            case "wall": Instantiate(wall, pos, Quaternion.Euler(rot)).transform.SetParent(world.transform); break;
+            case "orange": Instantiate(orange, pos, Quaternion.Euler(rot)).transform.SetParent(world.transform); break;
+            case "blue": Instantiate(blue, pos, Quaternion.Euler(rot)).transform.SetParent(world.transform); break;
+            case "red": Instantiate(red, pos, Quaternion.Euler(rot)).transform.SetParent(world.transform); break;
         } 
+    }
+
+    private void SaveWorldAsPrefab() 
+    {
+
+        string localPath = "Assets/Resources/" + "generatedWorld" + ".prefab";
+
+
+
+        PrefabUtility.SaveAsPrefabAsset(world, localPath);
+       
+        Destroy(world);
+        AssetDatabase.Refresh();
     }
 }
